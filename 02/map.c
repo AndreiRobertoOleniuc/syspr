2054755eb4f2587dd1952f35286545036a122d9c
Andrei Oleniuc (Pi) <andreioleniucroberto@gmail.com>
2026-04-18 11:49:53 +0000

    Refactor map and forEach functions for improved readability; add .gitignore to manage ignored files

--- 02/map.c
+++ 02/map.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
-void map(int a[], int len, int (*f)(int)) {
+void map(int a[], int len, int (*f)(int))
+{
     printf("map\n");
-    for (int i = 0; i < len; i++) {
-        a[i] = f(a[i]); 
+    for (int i = 0; i < len; i++)
+    {
+        a[i] = f(a[i]);
     }
 }
 
-int inc(int i) {
+void forEach(int a[], int len, void (*f)(int))
+{
+    printf("foreach\n");
+    for (int i = 0; i < len; i++)
+    {
+        f(a[i]);
+    }
+}
+
+int inc(int i)
+{
     printf("inc\n");
     return i + 1;
 }
 
-int main() {
+void display(int i)
+{
+    printf("This number was printed %d\n", i);
+}
+
+int main()
+{
     int a[] = {0, 0, 7};
     int len = sizeof a / sizeof a[0]; // # of elements
-    map(a, len, inc); // inc is a function pointer
-    printf("{%d, %d, %d}\n", a[0], a[1], a[2]);
+    map(a, len, inc);                 // inc is a function pointer$
+    forEach(a, len, display);
+    // printf("{%d, %d, %d}\n", a[0], a[1], a[2]);
 }
