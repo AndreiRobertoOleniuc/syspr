c0cd579f86e4eb1e6f40a76779099759981e559d
Andrei Oleniuc (Pi) <andreioleniucroberto@gmail.com>
2026-04-18 15:31:59 +0000

    Add error handling for multiple file openings in open.c

--- 03/open.c
+++ 03/open.c
@@ -4,11 +4,14 @@
 
 int main(void) {
     int fd = open("my.txt", O_RDWR);
-    if (fd == -1) {
+    int fd2 = open("my2.txt", O_RDWR);
+    if (fd == -1 || fd2 == -1) {
         perror("open"); // reads errno
         return -1;
     }
-    printf("file opened, fd = %d\n", fd);
+    printf("file opened, fd = %d\n", fd); // 3
+    printf("file opened, fd2 = %d\n", fd2); // 4
     close(fd);
+    close(fd2);
     return 0;
 }
